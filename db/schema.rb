48688d2dd62a503c9f0a7e3ca6100d5b5828855b
sherri010 <aminshahidi.sh@gmail.com>
2016-11-28 13:03:25 -0800

    removes paperclip to solve deployment issue

--- db/schema.rb
+++ db/schema.rb
@@ -50,10 +50,7 @@ ActiveRecord::Schema.define(version: 20161128011539) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "auth_token"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
+    t.string   "image"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
